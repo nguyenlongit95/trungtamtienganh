b13ae2bf78480637cc9d345cfb111fae5c9b61e2
longnct7090 <longnct7090@cowell.com.vn>
2022-06-13 10:43:39 +0700

    tmp fix carbon

--- app/Http/Controllers/Admin/LopHocController.php
+++ app/Http/Controllers/Admin/LopHocController.php
@@ -15,7 +15,6 @@ use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Support\ResponseHelper;
-use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
